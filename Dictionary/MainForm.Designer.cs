1bc48a8a0db7257a84da71f2a037610c167c3093
NumberFour8 <lukas.pohanka@inina.net>
2010-11-16 23:34:14 +0100

    Slovnik - prvni verze

---
+++ Dictionary/MainForm.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Dictionary
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.vyhledavaciBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.vymenit = new System.Windows.Forms.Button();
+            this.slovaB = new System.Windows.Forms.ListBox();
+            this.slovaA = new System.Windows.Forms.ListBox();
+            this.popisek_jazykB = new System.Windows.Forms.Label();
+            this.popisek_jazykA = new System.Windows.Forms.Label();
+            this.stavovyRadek = new System.Windows.Forms.StatusStrip();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.vyhledavaciBox);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(2, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(327, 48);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Vyhledávání";
+            // 
+            // vyhledavaciBox
+            // 
+            this.vyhledavaciBox.Location = new System.Drawing.Point(58, 20);
+            this.vyhledavaciBox.Name = "vyhledavaciBox";
+            this.vyhledavaciBox.Size = new System.Drawing.Size(260, 20);
+            this.vyhledavaciBox.TabIndex = 1;
+            this.vyhledavaciBox.TextChanged += new System.EventHandler(this.vyhledavaciBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Slovo: ";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.vymenit);
+            this.groupBox2.Controls.Add(this.slovaB);
+            this.groupBox2.Controls.Add(this.slovaA);
+            this.groupBox2.Controls.Add(this.popisek_jazykB);
+            this.groupBox2.Controls.Add(this.popisek_jazykA);
+            this.groupBox2.Location = new System.Drawing.Point(2, 57);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(327, 413);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Slova";
+            // 
+            // vymenit
+            // 
+            this.vymenit.Location = new System.Drawing.Point(243, 10);
+            this.vymenit.Name = "vymenit";
+            this.vymenit.Size = new System.Drawing.Size(75, 23);
+            this.vymenit.TabIndex = 4;
+            this.vymenit.Text = "Otočit";
+            this.vymenit.UseVisualStyleBackColor = true;
+            this.vymenit.Click += new System.EventHandler(this.vymenit_Click);
+            // 
+            // slovaB
+            // 
+            this.slovaB.FormattingEnabled = true;
+            this.slovaB.Location = new System.Drawing.Point(177, 56);
+            this.slovaB.Name = "slovaB";
+            this.slovaB.Size = new System.Drawing.Size(144, 342);
+            this.slovaB.TabIndex = 3;
+            // 
+            // slovaA
+            // 
+            this.slovaA.FormattingEnabled = true;
+            this.slovaA.Location = new System.Drawing.Point(13, 56);
+            this.slovaA.Name = "slovaA";
+            this.slovaA.Size = new System.Drawing.Size(143, 342);
+            this.slovaA.TabIndex = 2;
+            // 
+            // popisek_jazykB
+            // 
+            this.popisek_jazykB.AutoSize = true;
+            this.popisek_jazykB.Location = new System.Drawing.Point(174, 31);
+            this.popisek_jazykB.Name = "popisek_jazykB";
+            this.popisek_jazykB.Size = new System.Drawing.Size(44, 13);
+            this.popisek_jazykB.TabIndex = 1;
+            this.popisek_jazykB.Text = "Jazyk B";
+            // 
+            // popisek_jazykA
+            // 
+            this.popisek_jazykA.AutoSize = true;
+            this.popisek_jazykA.Location = new System.Drawing.Point(10, 31);
+            this.popisek_jazykA.Name = "popisek_jazykA";
+            this.popisek_jazykA.Size = new System.Drawing.Size(44, 13);
+            this.popisek_jazykA.TabIndex = 0;
+            this.popisek_jazykA.Text = "Jazyk A";
+            // 
+            // stavovyRadek
+            // 
+            this.stavovyRadek.Location = new System.Drawing.Point(0, 477);
+            this.stavovyRadek.Name = "stavovyRadek";
+            this.stavovyRadek.Size = new System.Drawing.Size(332, 22);
+            this.stavovyRadek.TabIndex = 2;
+            this.stavovyRadek.Text = "Připraven";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(332, 499);
+            this.Controls.Add(this.stavovyRadek);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Slovník";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox vyhledavaciBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.StatusStrip stavovyRadek;
+        private System.Windows.Forms.ListBox slovaB;
+        private System.Windows.Forms.ListBox slovaA;
+        private System.Windows.Forms.Label popisek_jazykB;
+        private System.Windows.Forms.Label popisek_jazykA;
+        private System.Windows.Forms.Button vymenit;
+    }
+}
+
---
+++ Dictionary/MainForm.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    public partial class MainForm : Form
+    {
+        // Seznam slov v jazyku
+        string[] jazykA_slova = { "ahoj", "domov", "střecha", "kolej", "škola", "chleba", "soumrak", "cukr", "dálnice", "kousat" };
+        string[] jazykB_slova = { "hello", "home", "roof", "rail", "school", "bread", "dusk", "sugar", "highway", "bite" };
+
+        // Názvy jazků
+        string   jazykA_nazev = "Čeština";
+        string   jazykB_nazev = "Angličtina";
+
+        /// <summary>
+        /// Konstruktor hlavního formuláře
+        /// </summary>
+        public MainForm()
+        {
+            InitializeComponent();
+
+            // Vypíšeme všechna slova jazyku A (pomocí cyklu foreach)
+            foreach (string prvek in jazykA_slova)
+            {
+                slovaA.Items.Add(prvek);
+            }
+
+            // Vypíšeme všechna slova jazyku B (pomocí metody AddRange, šetří místo)
+            slovaB.Items.AddRange(jazykB_slova);
+
+            // Nastavíme názvy jazyků
+            popisek_jazykA.Text = jazykA_nazev;
+            popisek_jazykB.Text = jazykB_nazev;
+        }
+
+        /// <summary>
+        /// Událost, která nastává když se změní text ve vyhledávacím boxu
+        /// </summary>
+        /// <param name="sender">Nějaký parametr</param>
+        /// <param name="e">Nějaký druhý parametr</param>
+        private void vyhledavaciBox_TextChanged(object sender, EventArgs e)
+        {
+            if (vyhledavaciBox.Text.Length > 0) // V případě, že délka textu ve vyhledávacím poli není nulová...
+            {
+                // Ušetříme místo, když si text ve vyhledávacím poli uložíme do nějaké proměnné s krátkým názvem
+                string tx = vyhledavaciBox.Text;
+
+                // Vyprázdníme oba seznamy slov
+                slovaA.Items.Clear();
+                slovaB.Items.Clear();
+
+                // Projdeme všechna slova v jazyce A...
+                for (int i = 0; i < jazykA_slova.Length; i++)
+                {
+                    // ...vybereme ta, která začínají na písmena zadaná ve vyhledávacím poli...
+                    if (jazykA_slova[i].Substring(0, (tx.Length < jazykA_slova[i].Length ? tx.Length : jazykA_slova[i].Length) ) == tx)
+                    {
+                        // ... a umístíme je do seznamů slov.
+                        slovaA.Items.Add(jazykA_slova[i]);
+                        slovaB.Items.Add(jazykB_slova[i]);
+                    }
+                }
+
+                // Pokud se v seznamech nachází už jenom jeden prvek, označíme ho...
+                if (slovaA.Items.Count == 1)
+                {
+                    slovaA.SetSelected(0, true);
+                    slovaB.SetSelected(0, true);
+                }
+                else // v opačném případě zruš všechna označení.
+                {
+                    slovaA.ClearSelected();
+                    slovaB.ClearSelected();
+                }
+            }
+            else // Když v políčku není žádný text, tak...
+            {
+                // vyčistíme seznamy slov obou jazyků...
+                slovaA.Items.Clear();
+                slovaB.Items.Clear();
+
+                // ...a znovu je tam přidáme
+                slovaA.Items.AddRange(jazykA_slova);
+                slovaB.Items.AddRange(jazykB_slova);
+            }
+
+        }
+
+        /// <summary>
+        /// Událost, která nastává když uživatel klikne na tlačítko
+        /// </summary>
+        /// <param name="sender">Nějaký pro nás nepotřebný parametr</param>
+        /// <param name="e">Nějaký pro nás nepotřebný parametr</param>
+        private void vymenit_Click(object sender, EventArgs e)
+        {
+            // Vyměníme slova jazyku A za slova jazyku B a naopak
+            string[] tmp = jazykA_slova;
+            jazykA_slova = jazykB_slova;
+            jazykB_slova = tmp;
+
+            // Vyčistíme seznamy slov obou jazyků...
+            slovaA.Items.Clear();
+            slovaB.Items.Clear();
+
+            // ...a znovu je tam přidáme
+            slovaA.Items.AddRange(jazykA_slova);
+            slovaB.Items.AddRange(jazykB_slova);
+
+            // Vyměníme i názvy jazyků...
+            string tmp2 = jazykA_nazev;
+            jazykA_nazev = jazykB_nazev;
+            jazykB_nazev = tmp2;
+
+            // ...a znovu je nastavíme
+            popisek_jazykA.Text = jazykA_nazev;
+            popisek_jazykB.Text = jazykB_nazev;
+        }
+
+    }
+}
